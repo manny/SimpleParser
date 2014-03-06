e282d780fb80a3ca0d5380881c0b847a959cccb3
Manny <mjl288@scarletmail.rutgers.edu>
2014-03-06 00:03:05 -0500

    fixed findFirstMatchIn

--- src/main/scala/com/twitter/sample/SimpleParser.scala
+++ src/main/scala/com/twitter/sample/SimpleParser.scala
@@ -4,10 +4,10 @@ case class SimpleParsed(id: Long, text: String)
 
 class SimpleParser {
   
-  val tweetRegex = "\"id\":(.*),\""text\":\"(.*)\"".r
+  val tweetRegex = "\"id\":(.*),\"text\":\"(.*)\"".r
 
   def parse(str: String) = {
-    tweetRegex.findFirstMatch(str) match {
+    tweetRegex.findFirstMatchIn(str) match {
         case Some(m) => {
         val id = str.substring(m.start(1), m.end(1)).toInt
         val text = str.substring(m.start(2), m.end(2))
