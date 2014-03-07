3814896bb720cdd219d1da81eb3f9b2cd4da8cb9
Manny <mjl288@scarletmail.rutgers.edu>
2014-03-06 23:25:54 -0500

    final clean up before ignore

--- src/test/scala/com/twitter/sample/SimpleParserSpec.scala
+++ src/test/scala/com/twitter/sample/SimpleParserSpec.scala
@@ -15,5 +15,32 @@ object SimpleParserSpec extends Specification {
         case _ => fail("didnt parse tweet")
       }
     }
+
+    "reject a non-JSON tweet" in {
+      val tweet = """"id":1,"text":"foo""""
+      parser.parse(tweet) match {
+        case Some(parsed) => fail("didn't reject a non-JSON tweet")
+        case e => e must be_==(None)
+      }
+    }
+
+    "ignore nested content" in {
+      val tweet = """{"id":1,"text":"foo","nested":{"id":2}}"""
+      parser.parse(tweet) match {
+        case Some(parsed) => {
+          parsed.text must be_==("foo")
+          parsed.id must be_==(1)
+        }
+        case _ => fail("didn't parse tweet")
+      }
+    }
+
+    "fail on partial content" in {
+      val tweet = """{"id":1}"""
+      parser.parse(tweet) match {
+        case Some(parsed) => fail("didn't reject a partial tweet")
+        case e => e must be_==(None)
+      }
+    }
   }
 }
