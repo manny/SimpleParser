ce63eb44c997d1a950ed0ee60b3cb863ec9b9715
Manny <mjl288@scarletmail.rutgers.edu>
2014-03-09 23:09:34 -0400

    added publishing in sampleproject

--- project/build/SampleProject.scala
+++ project/build/SampleProject.scala
@@ -1,9 +1,10 @@
 import sbt._
+import com.twitter.sbt._
 
-
-
-class SampleProject(info: ProjectInfo) extends DefaultProject(info) {
+class SampleProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   lazy val print = task {log.info("a test action"); None}
   val jackson = "org.codehaus.jackson" % "jackson-core-asl" % "1.6.1"
   val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven/")
 }
